a16af4c420ed676df5bdc8d61d3a8fb5d0ccf30f
Manoel Amâncio <87789158+ManoelASNeto@users.noreply.github.com>
2022-08-07 11:14:12 -0300

    update 1

--- lib/presentation/presentation/home_page.dart
+++ lib/presentation/presentation/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:buscador_de_ceps/injection_container.dart';
 import 'package:buscador_de_ceps/presentation/bloc/search_cep_bloc.dart';
+import 'package:buscador_de_ceps/presentation/bloc/search_cep_event.dart';
 import 'package:buscador_de_ceps/presentation/bloc/search_cep_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,21 +25,11 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Image.network(
-            'https://store-images.s-microsoft.com/image/apps.6607.13510798887520085.3b5999bd-0689-4a5d-b1fa-378e87bb83a5.ee076621-7430-46f1-a4ac-a0c442d69e58?mode=scale&q=90&h=200&w=200&background=%230078D7',
-          ),
-          BlocBuilder<SearchCepBloc, SearchCepState>(
-            builder: (context, state) {
-              return _search(state, context);
-            },
-          ),
-        ],
+    return SingleChildScrollView(
+      child: BlocBuilder<SearchCepBloc, SearchCepState>(
+        builder: (context, state) {
+          return _search(state, context);
+        },
       ),
     );
   }
@@ -45,9 +37,7 @@ class _HomePageState extends State<HomePage> {
   Widget _search(SearchCepState state, BuildContext context) {
     switch (state.status) {
       case SearchCepStatus.loading:
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
+        return _loading(state, context);
       case SearchCepStatus.ready:
         return _ready(state, context);
       default:
@@ -57,11 +47,86 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Widget _ready(SearchCepState state, BuildContext context) {
-    return Center(
+  Widget _loading(SearchCepState state, BuildContext context) {
+    return Material(
       child: Container(
-        child: Card(elevation: 10),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 240, 216, 2),
+              Color.fromARGB(255, 13, 71, 119),
+              Color.fromARGB(255, 0, 14, 26)
+            ],
+          ),
+        ),
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 150,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Buscador de',
+                      style: GoogleFonts.caveat(
+                        textStyle:
+                            const TextStyle(fontSize: 35, color: Colors.black),
+                      ),
+                    ),
+                    const TextSpan(
+                      text: ' CEP´s',
+                      style: TextStyle(color: Colors.black87, fontSize: 28),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              SizedBox(
+                height: 70,
+                width: MediaQuery.of(context).size.width - 20,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: controller,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration.collapsed(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
+                          ),
+                        ),
+                        hintText: 'Digite o seu CEP'),
+                    maxLength: 8,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  _searchBloc.add(CepEvent());
+                },
+                style: OutlinedButton.styleFrom(elevation: 5),
+                child: const Text('Pesquisar'),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _ready(SearchCepState state, BuildContext context) {
+    return const Center(
+      child: Card(elevation: 10),
     );
   }
 }
